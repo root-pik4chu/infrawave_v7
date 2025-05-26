23348056092499f11d15bb90236d626ca3555345
root <68701068+root-pik4chu@users.noreply.github.com>
2025-05-26 21:39:05 +0530

    all done shyad sab khatam ...

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -47,7 +47,7 @@ export default function ContactPage() {
   ];
 
   return (
-    <div className="md:h-[100vh] h-auto bg-gray-50 py-8 px-4 sm:px-6 lg:px-8 pt-[20vh]">
+    <div className=" min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8 pt-[20vh]">
       <div className="max-w-7xl mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
           {/* Left Column */}
